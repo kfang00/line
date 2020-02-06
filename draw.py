0ea4ffbc2250abe2fc680c0cb342ccea6962eed7
Kayla Fang <kfang00@stuy.edu>
2020-02-06 15:35:18 -0500

    started code

--- draw.py
+++ draw.py
@@ -1,4 +1,39 @@
 from display import *
 
 def draw_line( x0, y0, x1, y1, screen, color ):
-    pass
+    x = x0
+    y = y0
+    A = 
+    B =
+    d = 2A + B
+    while x <= x1:
+        plot (x, y)
+        if d > 0:
+            y = y + 1
+            d = d + 2B
+        x = x + 1
+        d = d + 2A
+    # d = 2A + B
+    # while x <= x1:
+    #     plot (x, y)
+    #     if d > 0:
+    #         y = y + 1
+    #         d = d + 2B
+    #     x = x + 1
+    #     d = d + 2A
+    # d = 2A + B
+    # while x <= x1:
+    #     plot (x, y)
+    #     if d > 0:
+    #         y = y + 1
+    #         d = d + 2B
+    #     x = x + 1
+    #     d = d + 2A
+    # d = 2A + B
+    # while x <= x1:
+    #     plot (x, y)
+    #     if d > 0:
+    #         y = y + 1
+    #         d = d + 2B
+    #     x = x + 1
+    #     d = d + 2A
