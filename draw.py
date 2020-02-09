2741c1b233c586540e958929fff77951f1c32b1e
Kayla Fang <kfang00@stuy.edu>
2020-02-09 18:39:46 -0500

    added slope

--- draw.py
+++ draw.py
@@ -5,36 +5,38 @@ def draw_line( x0, y0, x1, y1, screen, color ):
 	y = int(y0)
 	A = y1 - y
 	B = -1 * (x1 - x)
-	d = 2 * A + B #octant 1
-	while x <= x1:
-        	plot (screen, color, x, y)
-        	if d > 0:
-            		y = y + 1
-            		d = d + 2 * B
-        	x = x + 1
-        	d = d + 2 * A
-	d = A + 2 * B #octant 2
-	while y <= y1:
-        	plot (screen, color, x, y)
-        	if d < 0:
-            		x = x + 1
-           	 	d = d + 2 * A
-        	y = y + 1
-        	d = d + 2 * B
-	d = A - 2 * B #octant 7
-	while y >= y1:
-		plot (screen, color, x, y)
-		if d < 0: 
-	    		x = x + 1
-	    		d = d + 2 * A
-		y = y - 1
-		d = d - 2 * B
-	d = 2 * A - B #octant 8
-	while x <= x1:
-		plot (screen, color, x, y)
-		if d > 0: 
+	if (((y1-y)/(x1-x)) > 0):
+		d = 2 * A + B #octant 1
+		while x <= x1:
+        		plot (screen, color, x, y)
+        		if d > 0:
+            			y = y + 1
+            			d = d + 2 * B
+        		x = x + 1
+        		d = d + 2 * A
+		d = A + 2 * B #octant 2
+		while y <= y1:
+        		plot (screen, color, x, y)
+        		if d < 0:
+            			x = x + 1
+           	 		d = d + 2 * A
+        		y = y + 1
+        		d = d + 2 * B
+	if (((y1-y)/(x1-x)) < 0):
+		d = A - 2 * B #octant 7
+		while y >= y1:
+			plot (screen, color, x, y)
+			if d > 0: 
+	    			x = x + 1
+	    			d = d + 2 * A
 			y = y - 1
 			d = d - 2 * B
-		x = x + 1
-		d = d + 2 * A
+		d = 2 * A - B #octant 8
+		while x <= x1:
+			plot (screen, color, x, y)
+			if d > 0: 
+				y = y - 1
+				d = d - 2 * B
+			x = x + 1
+			d = d + 2 * A
 
