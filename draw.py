2887acd5f2f429f69b3b3a2ae4bba87c3d3681f5
Kayla Fang <kfang00@stuy.edu>
2020-02-09 19:37:45 -0500

    small change

--- draw.py
+++ draw.py
@@ -4,7 +4,7 @@ def draw_line( x0, y0, x1, y1, screen, color ):
 	x = int(x0)
 	y = int(y0)
 	A = y1 - y
-	B = -1 * (x1 - x)
+	B = x1 - x
 	hold = 0
 	if (x1-x) == 0:
 		hold = 1
@@ -20,31 +20,31 @@ def draw_line( x0, y0, x1, y1, screen, color ):
             			d = d + 2 * B
         		x = x + 1
         		d = d + 2 * A
-##	elif (slope > 1):
-##		d = A + 2 * B #octant 2
-##		while y <= y1:
-##        		plot (screen, color, x, y)
-##        		if d < 0:
-##            			x = x + 1
-##           	 		d = d + 2 * A
-##        		y = y + 1
-##        		d = d + 2 * B
-##	elif (slope < -1):
-##		d = A - 2 * B #octant 7
-##		while y >= y1:
-##			plot (screen, color, x, y)
-##			if d > 0: 
-##	    			x = x + 1
-##	    			d = d + 2 * A
-##			y = y - 1
-##			d = d - 2 * B
-##	else:
-##		d = 2 * A - B #octant 8
-##		while x <= x1:
-##			plot (screen, color, x, y)
-##			if d < 0: 
-##				y = y - 1
-##				d = d - 2 * B
-##			x = x + 1
-##			d = d + 2 * A
+	elif (slope > 1):
+		d = A + 2 * B #octant 2
+		while y <= y1:
+        		plot (screen, color, x, y)
+        		if d < 0:
+            			x = x + 1
+           	 		d = d + 2 * A
+        		y = y + 1
+        		d = d + 2 * B
+	elif (slope < -1):
+		d = A - 2 * B #octant 7
+		while y >= y1:
+			plot (screen, color, x, y)
+			if d > 0: 
+	    			x = x + 1
+	    			d = d + 2 * A
+			y = y - 1
+			d = d - 2 * B
+	else:
+		d = 2 * A - B #octant 8
+		while x <= x1:
+			plot (screen, color, x, y)
+			if d < 0: 
+				y = y - 1
+				d = d - 2 * B
+			x = x + 1
+			d = d + 2 * A
 
