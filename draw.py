2f4bb010cb78981f26620840632bd2784605a5f9
Kayla Fang <kfang00@stuy.edu>
2020-02-09 19:09:46 -0500

    fixing slope

--- draw.py
+++ draw.py
@@ -10,7 +10,8 @@ def draw_line( x0, y0, x1, y1, screen, color ):
 		hold = 1
 	else:
 		hold = (x1-x)
-	if (((y1-y)/hold) > 0):
+	slope = ((y1-y)/hold)
+	if (slope < 1):
 		d = 2 * A + B #octant 1
 		while x <= x1:
         		plot (screen, color, x, y)
@@ -19,6 +20,7 @@ def draw_line( x0, y0, x1, y1, screen, color ):
             			d = d + 2 * B
         		x = x + 1
         		d = d + 2 * A
+	else if (slope > 1):
 		d = A + 2 * B #octant 2
 		while y <= y1:
         		plot (screen, color, x, y)
@@ -27,7 +29,7 @@ def draw_line( x0, y0, x1, y1, screen, color ):
            	 		d = d + 2 * A
         		y = y + 1
         		d = d + 2 * B
-	if (((y1-y)/(x1-x)) < 0):
+	else if (slope < -1):
 		d = A - 2 * B #octant 7
 		while y >= y1:
 			plot (screen, color, x, y)
@@ -36,6 +38,7 @@ def draw_line( x0, y0, x1, y1, screen, color ):
 	    			d = d + 2 * A
 			y = y - 1
 			d = d - 2 * B
+	else:
 		d = 2 * A - B #octant 8
 		while x <= x1:
 			plot (screen, color, x, y)
