cb6e3c61f442d0d8e5ef947e96702448360ff348
Kayla Fang <kfang00@stuy.edu>
2020-02-11 01:38:48 -0500

    trying again

--- main.py
+++ main.py
@@ -35,13 +35,25 @@ c = [ 250, 0, 250 ]
 # draw_line(XRES/2, 0, XRES/2, YRES-1, s, c);
 x = 0
 while x < 200:
-    c = [ 250, 0, 23 ]
-    draw_line((XRES) - 2 * x, (YRES / 3) - 4 * x, XRES/2, 0, s, c);
-    draw_line(XRES/2, 0, (XRES / 4) + 3 * x, (YRES / 2) + x, s, c);
-    c = [ 231, 174, 34 ]
-    draw_line((XRES) + 2 * x, (YRES / 3) + 4 * x, 0, YRES/2, s, c);
-    draw_line(0, YRES/2, (XRES / 4) - 3 * x, (YRES / 2) - x, s, c);
-    #draw_line(250, 250, x, (YRES * 56) % 500, s, c);
+    draw_line(0, 0, x-1, YRES-1, s, c)
+    draw_line(0, 0, x-1, YRES / 2, s, c)
+    draw_line(x-1, YRES-1, 0, YRES / 2, s, c)
+    c[BLUE] = 255;
+    draw_line(0, YRES-1, x-1, 0, s, c);
+    draw_line(0, YRES-1, x-1, YRES/2, s, c);
+    draw_line(x-1, 0, 0, YRES/2, s, c);
+    c[RED] = 255;
+    c[GREEN] = 0;
+    c[BLUE] = 0;
+    draw_line(0, 0, x/2, YRES-1, s, c);
+    draw_line(x-1, YRES-1, x/2, 0, s, c);
+    c[BLUE] = 255;
+    draw_line(0, YRES-1, x/2, 0, s, c);
+    draw_line(XRES-1, 0, x/2, YRES-1, s, c);
+    c[BLUE] = 0;
+    c[GREEN] = 255;
+    draw_line(0, YRES/2, x-1, YRES/2, s, c);
+    draw_line(XRES/2, 0, x/2, YRES-1, s, c);
     x = x + 10
 
 display(s)
