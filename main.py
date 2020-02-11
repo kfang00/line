18b965172ad26911a9fb7c5104e4ed799fc344be
Kayla Fang <kfang00@stuy.edu>
2020-02-11 01:57:32 -0500

    small change8

--- main.py
+++ main.py
@@ -34,14 +34,14 @@ c = [ 250, 0, 250 ]
 # draw_line(0, YRES/2, XRES-1, YRES/2, s, c);
 # draw_line(XRES/2, 0, XRES/2, YRES-1, s, c);
 x = 0
-while x < 600:
+while x < 500:
     draw_line(0, 0, x+1, YRES-1, s, c)
-    draw_line(0, 0, x+1, YRES / 2 + x, s, c)
+    draw_line(0, 0, x+1, YRES / 2 - x, s, c)
     draw_line(x+1, YRES-1, 0, YRES / 2 + x, s, c)
     c[BLUE] = 255;
     draw_line(0, YRES-1, x+1, 0, s, c);
     draw_line(0, YRES-1, x+1, YRES/2 + x, s, c);
-    draw_line(x+1, 0, 0, YRES/2 + x, s, c);
+    draw_line(x+1, 0, 0, YRES/2 - x, s, c);
     c[RED] = 255;
     c[GREEN] = 0;
     c[BLUE] = 0;
@@ -52,7 +52,7 @@ while x < 600:
     draw_line(x+1, 0, x/2, YRES-1, s, c);
     c[BLUE] = 0;
     c[GREEN] = 255;
-    draw_line(0, YRES/2 + x, x+1, YRES/2 + x, s, c);
+    draw_line(0, YRES/2 - x, x+1, YRES/2 + x, s, c);
     draw_line(x/2, 0, x/2, YRES-1, s, c);
     x = x + 10
 
