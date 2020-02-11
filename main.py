639f62491ad944b02e59671718c4921d5eeb5fcd
Kayla Fang <kfang00@stuy.edu>
2020-02-11 01:17:21 -0500

    adding more lines

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from draw import *
 import math
 
 s = new_screen()
-c = [ 0, 0, 0 ]
+c = [ 250, 0, 250 ]
 
 # #octants 1 and 5
 # draw_line(0, 0, XRES-1, YRES-1, s, c)
@@ -35,11 +35,14 @@ c = [ 0, 0, 0 ]
 # draw_line(XRES/2, 0, XRES/2, YRES-1, s, c);
 x = 0
 while x < 200:
-    c[RED] += 1
+    c[RED] -= 1
     c[GREEN] += 1
-    draw_line((XRES) - 2 * x, (YRES / 3) + 4 * x, 250+x, 250+x, s, c);
+    draw_line((XRES) + 2 * x, (YRES / 3) + 4 * x, 250+x, 250+x, s, c);
     draw_line(250-x, 250-x, (XRES / 4) + 3 * x, (YRES / 2) + x, s, c);
-    draw_line((XRES / 4) + 3 * x, (YRES / 2) + x, (XRES) - 2 * x, (YRES / 3) + 4 * x, s, c);
+    draw_line((XRES / 4) + 3 * x, (YRES / 2) + x, (XRES) + 2 * x, (YRES / 3) + 4 * x, s, c);
+    draw_line((XRES) - 2 * x, (YRES / 3) - 4 * x, 250-x, 250-x, s, c);
+    draw_line(250+x, 250+x, (XRES / 4) - 3 * x, (YRES / 2) - x, s, c);
+    draw_line((XRES / 4) - 3 * x, (YRES / 2) - x, (XRES) - 2 * x, (YRES / 3) - 4 * x, s, c);
     #draw_line(250, 250, x, (YRES * 56) % 500, s, c);
     x = x + 10
 
