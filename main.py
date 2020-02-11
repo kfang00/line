9e80e3ae143eeec963e7133bb0e5bbae6352aa3d
Kayla Fang <kfang00@stuy.edu>
2020-02-10 23:39:08 -0500

    creative pic 2

--- main.py
+++ main.py
@@ -33,13 +33,11 @@ c = [ 0, 255, 0 ]
 # c[GREEN] = 255;
 # draw_line(0, YRES/2, XRES-1, YRES/2, s, c);
 # draw_line(XRES/2, 0, XRES/2, YRES-1, s, c);
-x = 0
-y = 0
-for x in range(500)
-    draw_line(250, 250, x, y * 56, s, c);
-    draw_line(x % 500, y, 250, 250, s, c);
+for x in range(500):
+    draw_line(250, 250, x, x * 56, s, c);
+    draw_line(x % 500, x, 250, 250, s, c);
     c[RED] = x
-    draw_line(XRES-1, 0, x * 3, y/5, s, c);
+    draw_line(XRES-1, 0, x * 3, x/5, s, c);
 
 
 display(s)
